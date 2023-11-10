d2b5b8f5bdd7cec65ff21dc7fc37dfa923aa1f32
alnah <alexis.nahan@gmail.com>
2023-11-10 19:15:25 -0300

    feat: going back to last working version

--- script.js
+++ script.js
@@ -1,66 +1,269 @@
 calculator();
 
 function calculator() {
-
-  // Element
-  const displayOperation = document.querySelector("#operation");
-  const displayResult = document.querySelector("#result");
-  const clearBtn = document.querySelector("#clear");
-  const oppositeBtn = document.querySelector("#opposite");
-  const percentageBtn = document.querySelector("#percentage");
-  const equalsBtn = document.querySelector("#equals");
-  const decimalBtn = document.querySelector("#decimal");
-  // Nodelist of elements
-  const numberBtns = document.querySelectorAll(".number");
-  const operatorBtns = document.querySelectorAll(".operator");
-
   let expression = {
-    first: "",
-    second: "",
-    operator: "",
-    reset: false,
-  };
-
-  function add(a, b) {
-    return a + b;
+    first: {value: "0", stored: false, decimal: false},
+    operator: {value: "0", stored: false},
+    second: {value: "0", stored: false, decimal: false, start: false},
+    result: {value: "0", stored: false},
   }
 
-  function subtract(a, b) {
-    return a - b;
+  listenClicks()
+  listenKeys()
+
+  function listenKeys() {
+    window.addEventListener("keydown", event => {
+      const keydown = document.querySelector(`.btn[data-key="${event.key}"]`);
+      if (keydown) {
+        bind(keydown.id);
+        keydown.focus();
+      }
+    });
   }
 
-  function multiplication(a, b) {
-    return a * b;
-  }
-
-  function power(a, b) {
-    return a ** b;
-  }
-
-  function divide(a, b) {
-    return a / b;
-  }
-
-  function operate(a, b, operator) {
-    a = Number(a);
-    b = Number(b);
-    switch (operator) {
-      case "+":
-        return add(a, b);
-      case "-":
-        return subtract(a, b);
-      case "*":
-        return multiplication(a, b);
-      case "^":
-        return power(a, b);
-      case "/":
-        if (b === 0) {
-          return null;
-        }
-        return divide(a, b);
-      default:
-        return null;
+  function listenClicks() {
+    const buttons = document.querySelectorAll(".btn");
+    for (let button of buttons) {
+      button.addEventListener("click", event => {
+        const click = event.target;
+        bind(click.id);
+        click.focus();
+      });
     }
   }
 
+  function bind(event) {
+    if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(event)) {
+      store(expression, event);
+      modifyClearBtn(expression);
+    }
+    if (["+", "-", "/", "*", "^"].includes(event)) {
+      chain(expression, event);
+    }
+    switch (event) {
+      case "=":
+        calculate(expression);
+        break;
+      case ".":
+        decimal(expression);
+        break;
+      case "+/-":
+        opposite(expression);
+        break;
+      case "%":
+        percentage(expression);
+        break;
+      case "ac":
+        clear(expression);
+        break;
+    }
+    display(expression, "0");
+    console.table(expression);
+  }
+
+  function store(expression, number) {
+    if (!expression.second.start) {
+      if (!expression.first.stored) {
+        expression.first.value = number;
+        expression.first.stored = true;
+      } else {
+        expression.first.value += number;
+      }
+    } else {
+      if (!expression.second.stored) {
+        expression.second.value = number;
+        expression.second.stored = true;
+      } else {
+        expression.second.value += number;
+      }
+    }
+  }
+
+  function modifyClearBtn(expression) {
+    const btn = document.querySelector("#ac");
+    if (expression.second.stored || expression.first.stored) {
+      btn.textContent = "C";
+    } else {
+      btn.textContent = "AC";
+    }
+  }
+
+  function chain(expression, operator) {
+    if (!expression.second.stored) {
+      expression.operator.value = operator;
+      expression.operator.stored = true;
+      expression.second.start = true;
+    } else {
+      expression.first.value = String(operate(expression));
+      expression.operator.value = operator;
+      reset([expression.second, expression.result]);
+    }
+  }
+
+  function calculate(expression) {
+    if (expression.second.stored && expression.first.stored) {
+      expression.result.value = String(operate(expression));
+      expression.result.stored = true;
+    } else if (expression.first.stored) {
+      expression.first.value = String(operate(expression));
+    }
+    convert(expression, String);
+  }
+
+  function decimal(expression) {
+    if (!expression.first.decimal) {
+      if (!expression.first.stored) {
+        expression.first.value = "0.";
+        expression.first.stored = true;
+      } else {
+        expression.first.value += ".";
+      }
+      expression.first.decimal = true;
+    }
+    if (expression.second.start) {
+      if (!expression.second.decimal) {
+        if (!expression.second.stored) {
+          expression.second.value = "0.";
+          expression.second.stored = true;
+        } else {
+          expression.second.value += ".";
+        }
+        expression.second.decimal = true;
+      }
+    }
+  }
+
+  function opposite(expression) {
+    if (expression.result.stored) {
+      expression.result.value = String(expression.result.value * -1);
+    } else if (expression.second.stored) {
+      expression.second.value = String(expression.second.value * -1);
+    } else if (expression.first.stored) {
+      expression.first.value = String(expression.first.value * -1);
+    }
+  }
+
+  function percentage(expression) {
+    if (expression.result.stored) {
+      expression.first.value = String(expression.result.value / 100);
+      reset([expression.second, expression.result]);
+    } else if (expression.second.stored) {
+      expression.second.value = String(expression.second.value / 100);
+    } else if (expression.first.stored) {
+      expression.first.value = String(expression.first.value / 100);
+    }
+  }
+
+  function clear(expression) {
+    const clearBtn = document.querySelector("#ac");
+    if (expression.result.stored) {
+      reset(expression);
+      clearBtn.textContent = "AC";
+    } else if (expression.second.stored) {
+      reset([expression.second]);
+      expression.second.start = true;
+    } else if (expression.operator.stored) {
+      reset([expression.operator])
+      expression.second.start = false;
+    } else if (expression.first.stored) {
+      reset([expression.first]);
+      clearBtn.textContent = "AC";
+    }
+  }
+
+  function reset(keys) {
+    for (let key in keys) {
+      keys[key].value = "0";
+      keys[key].stored = false;
+      if (["first", "second"].includes(key)) {
+        keys[key].decimal = false;
+      }
+      if (key === "second") {
+        keys[key].start = false;
+      }
+    }
+  }
+
+  function operate(expression) {
+    convert(expression, Number);
+    switch (expression.operator.value) {
+      case "+":
+        return add(expression);
+      case "-":
+        return subtract(expression);
+      case "*":
+        return multiply(expression);
+      case "/":
+        return divide(expression);
+      case "^":
+        return power(expression);
+    }
+  }
+
+  function add(expression) {
+    return !expression.second.stored
+      ? expression.first.value + expression.first.value
+      : expression.first.value + expression.second.value;
+  }
+
+  function subtract(expression) {
+    return !expression.second.stored
+      ? expression.first.value - expression.first.value
+      : expression.first.value - expression.second.value;
+  }
+
+  function multiply(expression) {
+    return !expression.second.stored
+      ? expression.first.value * expression.first.value
+      : expression.first.value * expression.second.value;
+  }
+
+  function divide(expression) {
+    return expression.second.stored
+      ? expression.second.value === 0
+        ? "Error"
+        : expression.first.value / expression.second.value
+      : expression.first.value / expression.first.value;
+  }
+
+  function power(expression) {
+    return !expression.second.stored
+      ? expression.first.value ** expression.first.value
+      : expression.first.value ** expression.second.value;
+  }
+
+  function round(number) {
+    const threshold = 1e6; // Scientific notation >= 1 million
+    const decimals = 6; // Limit to 6 digits after the decimal
+    if (Math.abs(number) >= threshold) {
+      return number.toExponential(3);
+    } else {
+      if (number % 1 !== 0) {
+        return Number(number.toFixed(decimals));
+      } else {
+        return number;
+      }
+    }
+  }
+
+  function display(expression, message) {
+    const display = document.querySelector("#display");
+    let value = message;
+    if (expression.result.stored) {
+      value = round(Number(expression.result.value));
+    } else if (expression.second.stored) {
+      value = round(Number(expression.second.value));
+    } else if (expression.first.stored) {
+      value = round(Number(expression.first.value));
+    }
+    display.textContent = value;
+  }
+
+  function convert(expression, builtin) {
+    for (let key in expression) {
+      if (["first", "second"].includes(key)) {
+        expression[key].value = builtin(expression[key].value);
+      }
+    }
+  }
 }
