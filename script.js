4bedc55fd85d8c396c7190647796b18d03990421
alnah <alexis.nahan@gmail.com>
2023-11-10 14:29:05 -0300

    feat: add operate function

--- script.js
+++ script.js
@@ -9,24 +9,46 @@ function calculator() {
     reset: false,
   }
 
-  function add(expression) {
-    return expression.first + expression.second;
+  function add(a, b) {
+    return a + b;
   }
 
-  function subtract(expression) {
-    return expression.first - expression.second;
+  function subtract(a, b) {
+    return a - b;
   }
 
-  function multiplication(expression) {
-    return expression.first * expression.second;
+  function multiplication(a, b) {
+    return a * b;
   }
 
-  function power(expression) {
-    return expression.first ** expression.second;
+  function power(a, b) {
+    return a ** b;
   }
 
-  function divide(expression) {
-    return expression.first / expression.second;
+  function divide(a, b) {
+    return a / b;
+  }
+
+  function operate(a, b, operator) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+      case "+":
+        return add(a, b);
+      case "-":
+        return subtract(a, b);
+      case "*":
+        return multiplication(a, b);
+      case "^":
+        return power(a, b);
+      case "/":
+        if (b === 0) {
+          return null;
+        }
+        return divide(a, b);
+      default:
+        return null;
+    }
   }
 
 }
